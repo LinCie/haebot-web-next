6dd1fd2bc1bf70ae2c635156c84a5161754a4aa0
LinCie <aldiofernanda@protonmail.com>
2024-11-09 11:07:56 +0700

    populate sidebar

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -12,31 +12,70 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
   SidebarGroupLabel,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
 
 export function AppSidebar() {
   return (
-    <Sidebar
-      className="block md:hidden"
-      variant="sidebar"
-      collapsible="offcanvas"
-    >
+    <Sidebar className="md:hidden" variant="sidebar" collapsible="offcanvas">
+      {/* Sidebar Header */}
       <SidebarHeader>
         <div>
           <Image
             src={haebotLogo}
             alt="HaeBot"
-            loading="eager"
+            loading="lazy"
             decoding="async"
             className="h-8 w-auto select-none"
           />
         </div>
       </SidebarHeader>
+
+      {/* Sidebar Content */}
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Navigation</SidebarGroupLabel>
           <SidebarMenu>
             {headerLinks.map((link) => {
+              // Render collapsible menu if content exists (child items)
+              if (link.content !== undefined) {
+                return (
+                  <Collapsible key={link.display} className="group/collapsible">
+                    <SidebarMenuItem>
+                      {/* Collapsible Toggle */}
+                      <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                          {link.icon}
+                          <span>{link.display}</span>
+                        </SidebarMenuButton>
+                      </CollapsibleTrigger>
+
+                      {/* Nested menu items */}
+                      <CollapsibleContent className="data-[state=open]:animate-slide-down data-[state=closed]:animate-slide-up overflow-hidden">
+                        <SidebarMenuSub>
+                          {link.content.map((content) => {
+                            return (
+                              <SidebarMenuSubItem key={content.display}>
+                                <a href={content.url}>
+                                  <span>{content.display}</span>
+                                </a>
+                              </SidebarMenuSubItem>
+                            );
+                          })}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </SidebarMenuItem>
+                  </Collapsible>
+                );
+              }
+
+              // Render a regular menu if content doesn't exist
               return (
                 <SidebarMenuItem key={link.display}>
                   <SidebarMenuButton asChild>
